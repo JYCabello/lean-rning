311e587560d51e8baa276e72e1b0628968bcfe8d
Yeray Cabello <j.cabello.ramos@gmail.com>
2025-12-10 19:29:18 +0100

    Add initial definitions for Maybe and NumberGreaterThan types, and implement related functions

--- LeanRning/Basic.lean
+++ LeanRning/Basic.lean
@@ -1,1 +1,26 @@
 def hello := "world"
+
+inductive Maybe (α : Type) : Type
+| nothing : Maybe α
+| just    : α → Maybe α
+
+def positive (num: Int) : Maybe Nat :=
+  match num with
+  | Int.ofNat (Nat.succ n) => Maybe.just (n + 1)
+  | _                      => Maybe.nothing
+
+#eval positive (5)
+
+inductive NumberGreaterThan (num : Int) : Type
+| just (val : Int) (h : val > num) : NumberGreaterThan num
+| nothing : NumberGreaterThan num
+
+def greaterThan (num : Int) (val : Int) : NumberGreaterThan num :=
+  if   h : val > num
+  then NumberGreaterThan.just val h
+  else NumberGreaterThan.nothing
+
+def greaterThanTen := greaterThan 10
+
+#eval greaterThanTen 15
+#eval greaterThanTen 5
