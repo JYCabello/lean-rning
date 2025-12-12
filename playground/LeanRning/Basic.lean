17147309b00482d49804bc4eb6430b3cd82cb4aa
Yeray Cabello <j.cabello.ramos@gmail.com>
2025-12-12 20:34:01 +0100

    .

--- playground/LeanRning/Basic.lean
+++ playground/LeanRning/Basic.lean
@@ -1,3 +1,5 @@
+import Std.Tactic.Do
+
 def hello := "world"
 
 inductive Maybe (α : Type) : Type
@@ -11,19 +13,24 @@ def positive (num: Int) : Maybe Nat :=
 
 #eval positive (5)
 
+abbrev IsGreaterThan (num : Int) (val: Int) : Prop := val > num
+
 inductive NumberGreaterThan (num : Int) : Type
-| just (val : Int) (h : val > num)   : NumberGreaterThan num
-| nothing : NumberGreaterThan num
+| just (val : Int) (h : IsGreaterThan num val) : NumberGreaterThan num
 
-def greaterThan (num : Int) (val : Int) : NumberGreaterThan num :=
-  if   h: val > num
-  then NumberGreaterThan.just val h
-  else NumberGreaterThan.nothing
+instance : Repr (NumberGreaterThan num) where
+  reprPrec x _ := match x with
+    | NumberGreaterThan.just val _ => s!"NumberGreaterThan {num} < {val}"
 
-def greaterThanPatterned (num : Int) (val : Int) : NumberGreaterThan num :=
-  match h : decide (val > num) with
-  | true  => NumberGreaterThan.just val (of_decide_eq_true h)
-  | false => NumberGreaterThan.nothing
+def greaterThan (num : Int) (val : Int) : Option (NumberGreaterThan num) :=
+  if   h: IsGreaterThan num val
+  then some <| NumberGreaterThan.just val h
+  else none
+
+def greaterThanPatterned (num : Int) (val : Int) : Option (NumberGreaterThan num) :=
+  match h : decide (num < val) with
+  | true  => some <| NumberGreaterThan.just val (of_decide_eq_true h)
+  | false => none
 
 def greaterThanTen := greaterThan 10
 
