340cf5ffc6a89a364632c1007354850d34020f77
Yeray Cabello <j.cabello.ramos@gmail.com>
2025-12-24 23:20:17 +0100

    feat: add NonEmptyList and Validate structures with associated instances

--- playground/LeanRning/Applicatives.lean
+++ playground/LeanRning/Applicatives.lean
@@ -13,3 +13,41 @@ def tryValidPerson (name : String) (year : Nat) : Option (CheckedInput 2025) :=
       some { name := ⟨name, hName⟩, birthYear := ⟨year, hYear⟩ }
   else none
   else none
+
+structure NonEmptyList (α : Type) : Type where
+  head : α
+  tail : List α
+
+def NonEmptyList.append (xs ys : NonEmptyList α) : NonEmptyList α :=
+  ⟨xs.head, xs.tail ++ [ys.head] ++ ys.tail⟩
+
+instance : Append (NonEmptyList α) where
+  append xs ys :=
+    { head := xs.head, tail := xs.tail ++ ys.head :: ys.tail }
+
+instance : HAppend (NonEmptyList α) (List α) (NonEmptyList α) where
+  hAppend xs ys :=
+    { head := xs.head, tail := xs.tail ++ ys }
+
+inductive Validate (ε α : Type) : Type where
+  | ok : α → Validate ε α
+  | errors : NonEmptyList ε → Validate ε α
+
+instance : Functor (Validate ε) where
+  map f
+   | .ok x => .ok (f x)
+   | .errors errs => .errors errs
+
+instance : Applicative (Validate ε) where
+  pure := .ok
+  seq f x :=
+    match f with
+    | .ok g => g <$> (x ()) -- Same as `Functor.map g (x ())`
+    | .errors errs =>
+      match x () with
+      | .ok _ => .errors errs
+      | .errors errs' => .errors (errs ++ errs')
+
+def Field := String
+def reportError (f : Field) (msg : String) : Validate (Field × String) α :=
+  .errors { head := (f, msg), tail := [] }
